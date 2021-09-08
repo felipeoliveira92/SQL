4d415781c14ff25c0560c1e1f975748515681741
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2021-09-08 20:29:47 -0300

    Update 2613.sql

--- 2613.sql
+++ 2613.sql
@@ -1,1 +1,6 @@
+/*
+Antigamente a locadora fez um evento em que vários filmes estavam em promoção, queremos saber que filmes eram esses.
+
+Seu trabalho para nós ajudar é selecionar o ID e o nome dos filmes cujo preço for menor que 2.00.
+*/
 SELECT m.id, m.name from movies m INNER JOIN prices p on m.id_prices = p.id and p.value < 2.00
